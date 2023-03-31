8a8991b1668d62e794c891955318a6235358d4c6
MIRAGESYNTH <miragesynth@gmail.com>
2023-03-31 19:02:28 +0200

    Massive commit with new styling

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
 
 const parser = new DOMParser();
 
+// Sitewide Navigation
+function goTo(link) {
+  console.log(link);
+}
+
+
 // Slides creation
 
 let content = null;
@@ -52,23 +58,38 @@ function createMVs() {
     copy.querySelector('.mv-text-presentation').innerHTML = content[key].description;
     copy.querySelector('.mv-trailer-video').src = content[key].video;
 
+    let linkButtons = ["link-zero", "link-first", "link-second", "link-third"];
+    let currentLink = 0;
+
     if (content[key].options.page) {
-      copy.querySelector('.mv-buttons.page').classList.add('available');
-      copy.querySelector('.mv-button-text.page').href = content[key].references.page;
-    };
+      currentLink += 1
+      copy.querySelector('.mv-page-link').onclick = function() { goTo(content[key].references.page); };
+      copy.querySelector('.mv-page-link').classList.add(linkButtons[currentLink]);
+    } else {
+        copy.querySelector('.mv-page-link').remove();
+      };
+
     if (content[key].options.store) {
-      copy.querySelector('.mv-buttons.store').classList.add('available');
-      copy.querySelector('.mv-button-text.store').href = content[key].references.store;
-    };
-    if (content[key].options.android) {
-      copy.querySelector('.mv-buttons.android').classList.add('available');
-      copy.querySelector('.mv-button-text.android').href = content[key].references.android;
-    };
+      currentLink += 1
+      copy.querySelector('.mv-store-link').onclick = function() { goTo(content[key].references.store); };
+      copy.querySelector('.mv-store-link').classList.add(linkButtons[currentLink]);
+    } else {
+        copy.querySelector('.mv-store-link').remove();
+      };
+
+    if (content[key].options.mobile) {
+      currentLink += 1
+      copy.querySelector('.mv-mobile-link').onclick = function() { goTo(content[key].references.mobile); };
+      copy.querySelector('.mv-mobile-link').classList.add(linkButtons[currentLink]);
+    } else {
+        copy.querySelector('.mv-mobile-link').remove();
+      };
 
     if (key == defaultview()){
       copy.classList.add('show');
-    };
-    document.querySelector('.main-view').appendChild(copy)  
+    }
+    document.querySelector('.main-view').appendChild(copy);
+
   };
   return views;
 };
@@ -79,15 +100,15 @@ let prevButton = null;
 let nextButton = null;
 let currentSlide = 0;
 
-
 async function loadButtons() {
   if (!prevButton || !nextButton || !slides) {
-    prevButton = await document.querySelector(".prev-button");
     nextButton = await document.querySelector(".next-button");
+    nextButton.addEventListener("click", nextSlide);
+    prevButton = await document.querySelector(".prev-button");
+    prevButton.addEventListener("click", prevSlide);
     slides = document.querySelectorAll(".mv-container");
   };
 };
-
 
 function showSlide() {
   slides.forEach((slide, index) => {
@@ -117,58 +138,128 @@ function prevSlide() {
 
 // Blog previews
 
-let blogContent = null;
-let blogTemplate = null;
+async function addBlogPreview() {
 
-async function getBlogContent() {
-  if (!blogContent) {
-    // Wait for fetch to be over
-    const response = await fetch('./posts/posts.json');
-    const data = await response.json();  
-      // assign the JSON data to a variable
-      return data.posts;
-  };
-  return blogContent;
-};
+  const response = await fetch('./posts/posts.json');
+  const data = await response.json();  
+  let blogContent = data.posts;
 
-async function getBlogTemplate() {
-if (!blogTemplate) {
-  // Wait for fetch to be over
-  const response = await fetch('./posts/preview.html');
-  const html = await response.text();  
-    // assign the HTML div to a variable
-    const doc = parser.parseFromString(html, 'text/html');
-    return doc.querySelector('.blog-post-preview');
-};
-return blogTemplate;
-};
+  let blogPreviews = [];
 
- function createBlogPreview() {
-  let views = [];
-  console.log("starting operation");
   for (let key in blogContent) {
-    console.log("looping");
-    console.log(blogTemplate);
-    var copy = blogTemplate.cloneNode(true);
-    document.querySelector('.blog-preview-container').appendChild(copy);
-    console.log("added child");  
+    blogPreviews.push(key);
   };
-  return views;
-};
 
+  blogPreviews.reverse();
+
+  if ( blogPreviews.length > 8) {
+      blogPreviews.length = 8;
+    }
+
+  let blogTemplate = document.querySelector('.bpp');
+
+  const dictionary = ["zero", "one", "two", "three", "four", "five", "six", "seven"];
+  for (let i = 0; i < blogPreviews.length; i++) {
+    const copy = blogTemplate.cloneNode(true);
+    copy.classList.add(dictionary[i]);
+    document.querySelector('.bpc-articles').appendChild(copy);
+  };
+
+  blogTemplate.remove();
+
+};
 
 
 document.addEventListener("DOMContentLoaded", async function () {
     content = await getContent();
     template = await getTemplate();
     createMVs();
-    await loadButtons();
-    nextButton.addEventListener("click", nextSlide);
-    prevButton.addEventListener("click", prevSlide);
+    currentSlide = parseInt(defaultview());
 
-    blogContent = await getBlogContent();
-    blogTemplate = await getBlogTemplate();
-    createBlogPreview();
+    await addBlogPreview()
+    await loadButtons();
+
+
+
+    resizeElements();
+    activateElements();
+
+
+  });
+
+
+window.addEventListener('resize', function(event){
+  resizeElements();
 });
 
 
+function resizeElements() {
+
+  const r = document.querySelector(':root');
+  r.style.setProperty('--window-size-width', window.innerWidth);
+  const navBackground = document.querySelector('.nav-background');
+  r.style.setProperty('--header-background-size', `${navBackground.height}px`);
+  const navLink = document.querySelector('.nav-link');
+  r.style.setProperty('--link-button-size', `${navLink.width}px`);
+  const navIcon = document.querySelector('.nav-icon');
+  r.style.setProperty('--icon-button-size', `${navIcon.width}px`);
+  const separator = document.querySelector('.separator-background');
+  r.style.setProperty('--separator-background-size', `${separator.height}px`);
+  const mainView = document.querySelector('.main-view-background');
+  r.style.setProperty('--main-view-background-size', `${mainView.height}px`);
+
+  const title = document.querySelector('.mv-title-background');
+  r.style.setProperty('--mv-title-background-size', `${title.height}px`);
+  const overlay = document.querySelector('.mv-overlay-background');
+  r.style.setProperty('--mv-overlay-background-size', `${overlay.height}px`);
+  const text = document.querySelector('.mv-text-background');
+  r.style.setProperty('--mv-text-background-size', `${text.width}px`);
+  const trailer = document.querySelector('.mv-trailer-background');
+  r.style.setProperty('--mv-trailer-background-size', `${trailer.width}px`);
+  const links = document.querySelector('.mv-links-background');
+  r.style.setProperty('--mv-links-background-size', `${links.width}px`);
+  const link = document.querySelector('.mv-page-link');
+  r.style.setProperty('--mv-link-background-size', `${link.height}px`);
+
+  const secondaryView = document.querySelector('.news-bg');
+  r.style.setProperty('--secondary-view-background-size', `${secondaryView.height}px`);
+
+  const twitterContainer = document.querySelector('.twitter-container-background');
+  r.style.setProperty('--twitter-container-background-size', `${twitterContainer.width}px`);
+
+  const footerBackground = document.querySelector('.footer-bar-background');
+  r.style.setProperty('--footer-bar-background-size', `${footerBackground.height}px`);
+
+  const blogElements = [
+    "bpc",
+    "bpc-title",
+    "bpc-articles",
+    "bpc-nav",
+    "bpp",
+    "bpp-cover", 
+    "bpp-category", 
+    "bpp-date", 
+    "bpp-title", 
+    "bpp-button"
+  ];
+
+  blogElements.forEach((element) => {
+    setSizeCSS(element);
+  });
+
+};
+
+function setSizeCSS(name) {
+  const r = document.querySelector(':root');
+  console.log(`.${name}`);
+  var element = document.querySelector(`.${name}-bg`);
+  console.log(`--${name}-x`, `${element.width}px`)
+  r.style.setProperty(`--${name}-x`, `${element.width}px`)
+  console.log(`--${name}-y`, `${element.height}px`)
+  r.style.setProperty(`--${name}-y`, `${element.height}px`)
+};
+
+function activateElements() {
+  const r = document.querySelector(':root');
+  r.style.setProperty('--show', 1);
+};
